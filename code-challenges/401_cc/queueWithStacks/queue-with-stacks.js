06fa5532c75e28405f1c05c625e2610b8a8ea778
rebecca-pete <rebecca@rebeccapeterson.us>
2019-05-28 23:56:07 -0700

    pseudoQueue class

---
+++ code-challenges/401_cc/queueWithStacks/queue-with-stacks.js
@@ -0,0 +1,20 @@
+'use strict';
+
+class PseudoQueue {
+    constructor () {
+        this.length = 0;
+        this.storage = [];
+        this.stackA = {};
+        this.stackB = {};
+    }
+
+    enqueue(value) {
+    
+        
+
+    }
+
+    dequeue() {
+
+    }
+}
