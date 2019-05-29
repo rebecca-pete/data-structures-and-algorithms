0fa80f59878ce178de11031f5808bdfc21fa58f0
rebecca-pete <rebecca@rebeccapeterson.us>
2019-05-29 00:19:58 -0700

    queue-with-stacks

--- code-challenges/401_cc/queueWithStacks/queue-with-stacks.js
+++ code-challenges/401_cc/queueWithStacks/queue-with-stacks.js
@@ -3,18 +3,21 @@
 class PseudoQueue {
     constructor () {
         this.length = 0;
-        this.storage = [];
-        this.stackA = {};
-        this.stackB = {};
+        // this.storage = [];
+        this.stackA = [];
+        this.stackB = [];
     }
 
     enqueue(value) {
-    
-        
-
+        this.stackA.push(value);
+        return stackA;
     }
 
     dequeue() {
+        if(stackA.length > 0){
+            this.stackB.push(stackA[0]);
+            return stackB;
+        }
 
     }
 }
