f3d37488a77a54db5d40de12d62fb652ea841293
rebecca-pete <rebecca@rebeccapeterson.us>
2019-06-05 23:46:10 -0700

    adding methods to tree clases

--- code-challenges/401_cc/tree/tree.js
+++ code-challenges/401_cc/tree/tree.js
@@ -1,34 +1,78 @@
 'use strict';
 
 class Node {
-    constructor () {
+    constructor (node) {
         this.value = value;
         this.leftChild = leftChild;
         this.rightChild = rightChild;
     }
 }
 class Binary Tree {
-    constructor () {
-        this.value = value;
+    constructor (node) {
+      
     }
-    preOrder {
-
+    //Root, Left, Right
+    preOrder (node.value) {
+      if(node.value !== null) {
+        console.log(node.value);
+        preOrder(node.leftChild);
+        preOrder(node.rightChild);
+        return;
+      };
     },
-    inOrder {
-
+    //Left, Root, Right
+    inOrder (node.value) {
+      if (node.leftChild !== null) {
+        inOrder(node.leftChild);
+      }
+      console.log(node.value);
+      if (node.rightChild !== null) {
+        inOrder(node.rightChild);
+      }
     },
-    postOrder {
-
+    //Left, Right, Root
+    postOrder (node.value) {
+      if (node.leftChild !== null) {
+        inOrder(node.leftChild);
+      }
+      if (node.rightChild !== null) {
+        inOrder(node.rightChild);
+      }
+      console.log(node.value);
     },
 }
 class Binary Search Tree {
-    constructor () {
-        this.value = value;
-    }
-    add(value) {
+    constructor (node) {
 
-    },
-    contains(value) {
+      add (node.value) {
+        if(node.value === null){
+          return null;
+        }
+        while (!node.leftChild && !node.rightChild) {
+          let node.leftChild = node.value;
+        }
+        while (node.leftChild < node.value){
+          let node.rightChild = node.value;
+          }
+        while (node.rightChild > node.value){
+          let node.leftChild = node.value;
+        }
+      };
+      //need to finish
+    contains(node.value) {
+      if(node.value !== null){
+        if(node.value === root)
+      }
+      // while (!node.leftChild && !node.rightChild) {
+      //   if(node.leftChild === node.value){
+      //     return node;
+      //   }
+
+      // while(node.value > currentNode.value){
+      //   let node.rightChild = node.value;
+        
+      }
+
 
     }
 }
